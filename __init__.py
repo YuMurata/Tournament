defdee6dbac6118bc8cfc5314cc345192cd2c782
purin52002 <purin7635@gmail.com>
2019-12-22 22:30:05 +0900

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .tournament import Tournament, TournamentException
+from .tournament import GameWin, CompleteException
+from .player import Player
+from .evaluator import Evaluator
+from . import DataWriter
